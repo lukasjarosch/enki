9169ad4bed74745d4f572884a05462d1bf252e90
lukas <lukas.jarosch@mail.com>
2019-10-20 17:59:31 +0200

    add godin/amqp and port to enki
    Signed-off-by: lukas <lukas.jarosch@mail.com>

--- rabbitmq/connection.go
+++ rabbitmq/connection.go
@@ -1,1 +1,147 @@
 package rabbitmq
+
+import (
+	"context"
+	"sync"
+	"time"
+
+	"github.com/pkg/errors"
+	"github.com/streadway/amqp"
+	"go.uber.org/zap"
+)
+
+// Connection is a wrapper for amqp.Connection but adding reconnection functionality.
+type Connection struct {
+	addr                  string
+	conn                  *amqp.Connection
+	connMutex             sync.Mutex
+	logger                *zap.Logger
+	ctx                   context.Context
+	cancel                context.CancelFunc
+	connected             bool
+	notifyCloseConnection chan *amqp.Error
+}
+
+const ReconnectDelay = 5 * time.Second
+
+func NewConnection(addr string, logger *zap.Logger) *Connection {
+	ctx, cancel := context.WithCancel(context.Background())
+	c := &Connection{
+		ctx:                   ctx,
+		logger:                logger,
+		cancel:                cancel,
+		addr:                  addr,
+		connMutex:             sync.Mutex{},
+		notifyCloseConnection: make(chan *amqp.Error),
+	}
+
+	return c
+}
+
+// Connect will dial to the specified AMQP server addr.
+func (c *Connection) Connect() (err error) {
+	c.conn, err = c.dial()
+	if err != nil {
+		return errors.Wrap(err, "unable to connect to amqp server")
+	}
+
+	go c.monitorConnection()
+
+	return nil
+}
+
+// Shutdown the reconnector and terminate any existing connections
+func (c *Connection) Shutdown() {
+	c.setConnected(false)
+	c.cancel()
+
+	if c.IsConnected() {
+		err := c.conn.Close()
+		if err != nil {
+			c.logger.Warn("error while closing amqp connection", zap.Error(err))
+			return
+		}
+	}
+}
+
+// dial and return the connection and any occurred error
+func (c *Connection) dial() (*amqp.Connection, error) {
+	c.setConnected(false)
+
+	conn, err := amqp.Dial(c.addr)
+	if err != nil {
+		return nil, err
+	}
+	c.changeConnection(conn)
+	c.setConnected(true)
+	return conn, nil
+}
+
+// monitorConnection ensures that the amqp connection is recovered on failures.
+// if an error can be read from the amqp connectionClosed channel, then reconnect() is called
+func (c *Connection) monitorConnection() {
+	for {
+		select {
+		case <-c.ctx.Done():
+			return
+		case amqpErr, ok := <-c.notifyCloseConnection:
+			if ok {
+				c.logger.Warn("amqp connection error",
+					zap.String("err.reason", amqpErr.Reason),
+					zap.Int("err.code", amqpErr.Code))
+				c.reconnect()
+			}
+		}
+	}
+}
+
+// reconnect will, once started, try to connect to amqp forever
+// the method only returns if a connection is established or the ctxReconnect context is cancelled by Shutdown()
+func (c *Connection) reconnect() {
+	var err error
+
+	for {
+		select {
+		case <-c.ctx.Done():
+			return
+		default:
+		}
+		c.conn, err = c.dial()
+		if err != nil {
+			c.logger.Warn("unable to connect to amqp server", zap.Error(err))
+			time.Sleep(ReconnectDelay)
+			continue
+		}
+		c.logger.Info("reconnected to amqp server")
+		c.setConnected(true)
+		return
+	}
+}
+
+// changeConnection sets a new amqp.Connection and renews the notification channel
+func (c *Connection) changeConnection(connection *amqp.Connection) {
+	c.connMutex.Lock()
+	defer c.connMutex.Unlock()
+
+	c.conn = connection
+	c.notifyCloseConnection = make(chan *amqp.Error)
+	c.conn.NotifyClose(c.notifyCloseConnection)
+}
+
+func (c *Connection) IsConnected() bool {
+	c.connMutex.Lock()
+	defer c.connMutex.Unlock()
+	return c.connected
+}
+
+func (c *Connection) setConnected(status bool) {
+	c.connMutex.Lock()
+	defer c.connMutex.Unlock()
+	c.connected = status
+}
+
+func (c *Connection) Channel() (*amqp.Channel, error) {
+	c.connMutex.Lock()
+	defer c.connMutex.Unlock()
+	return c.conn.Channel()
+}
--- rabbitmq/session.go
+++ rabbitmq/session.go
@@ -1,1 +1,223 @@
 package rabbitmq
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/golang/protobuf/proto"
+	"github.com/streadway/amqp"
+	"go.uber.org/zap"
+)
+
+type Publisher interface {
+	Publish(routingKey string, event interface{}) error
+}
+
+type PublishExchange string
+
+type Session struct {
+	addr          string
+	ctx           context.Context
+	cancel        context.CancelFunc
+	logger        *zap.Logger
+	subscribers   map[string]Subscriber
+	publishers    map[string]PublishExchange
+	consumerQueue string
+	consumeConn   *Connection
+	produceConn   *Connection
+	consumerDecls []Declaration
+	producerDecls []Declaration
+}
+
+func NewSession(addr string, logger *zap.Logger) *Session {
+	ctx, cancel := context.WithCancel(context.Background())
+	s := &Session{
+		addr:          addr,
+		ctx:           ctx,
+		cancel:        cancel,
+		logger:        logger,
+		subscribers:   make(map[string]Subscriber),
+		publishers:    make(map[string]PublishExchange),
+		consumerQueue: "",
+	}
+
+	return s
+}
+
+// AddSubscription is a wrapper which uses the Auto*() functions
+// to quickly add an exchange, queue and binding to the declarations list.
+// It will also register the subscriber handler function with the subscriber map.
+// If no connection for the consumer exist, the connection is established
+// at this point. This happens only once, even if you add multiple subscriptions.
+func (s *Session) AddSubscription(exchangeName, queueName, routingKey string, handler Subscriber) error {
+	if s.consumerQueue != "" && s.consumerQueue != queueName {
+		return fmt.Errorf("a consumer queue with name '%s' has already been defined", s.consumerQueue)
+	}
+	s.consumerQueue = queueName
+	s.consumerDecls = append(s.consumerDecls, AutoExchange(exchangeName))
+	s.consumerDecls = append(s.consumerDecls, AutoQueue(queueName))
+	s.consumerDecls = append(s.consumerDecls, AutoBinding(routingKey, queueName, exchangeName))
+	s.subscribers[routingKey] = handler
+
+	s.logger.Info("added subscription",
+		zap.String("exchange", exchangeName),
+		zap.String("queue", queueName),
+		zap.String("routingKey", routingKey))
+	return nil
+}
+
+// AddPublisher is a wrapper to convenitently prepare the session for publishing on a specific exchange.
+// The method ensures that the target exchange is declared when calling Declare().
+func (s *Session) AddPublisher(exchangeName, routingKey string) error {
+	if _, exists := s.publishers[routingKey]; exists {
+		return fmt.Errorf("a publisher with that routingKey is already registered")
+	}
+	s.producerDecls = append(s.producerDecls, AutoExchange(exchangeName))
+	s.publishers[routingKey] = PublishExchange(exchangeName)
+
+	return nil
+}
+
+// Publish will take the event, marshall it into a proto.Message and then send it on it's journey
+// to the spe
+func (s *Session) Publish(routingKey string, event interface{}) error {
+	exchange, ok := s.publishers[routingKey]
+	if !ok {
+		return fmt.Errorf("no publisher with routingKey %s registered, cannot resolve exchange", routingKey)
+	}
+
+	protobuf := event.(proto.Message)
+	bodyBytes, err := proto.Marshal(protobuf)
+	if err != nil {
+		return err
+	}
+	publishing := amqp.Publishing{
+		Headers:      amqp.Table{},
+		ContentType:  "application/octet-stream",
+		DeliveryMode: amqp.Transient,
+		Priority:     0,
+		Body:         bodyBytes,
+	}
+
+	ch, err := s.produceConn.Channel()
+	if err != nil {
+		return err
+	}
+
+	if err := ch.Publish(string(exchange), routingKey, false, false, publishing); err != nil {
+		return err
+	}
+
+	s.logger.Info(fmt.Sprintf("published message to exchange %s with routingKey %s", exchange, routingKey),
+		zap.String("exchange", string(exchange)),
+		zap.String("routingKey", routingKey))
+
+	return nil
+}
+
+// ensureConnections will ensure that for any configured consumer or producer declarations,
+// a connection exists and is online.
+func (s *Session) ensureConnections() error {
+	if len(s.consumerDecls) > 0 && s.consumeConn == nil {
+		s.consumeConn = NewConnection(s.addr, s.logger.Named("consumer"))
+		if err := s.consumeConn.Connect(); err != nil {
+			return fmt.Errorf("failed to create amqp connection: %s", err)
+		}
+		s.logger.Info("amqp consumer connection established")
+	}
+	if len(s.producerDecls) > 0 && s.produceConn == nil {
+		s.produceConn = NewConnection(s.addr, s.logger.Named("producer"))
+		if err := s.produceConn.Connect(); err != nil {
+			return fmt.Errorf("failed to create amqp connection: %s", err)
+		}
+		s.logger.Info("amqp producer connection established")
+	}
+	return nil
+}
+
+// Declare goes through all declarations and uses the consumer/produce connection to
+// obtain a channel and perform the declarations.
+func (s *Session) Declare() error {
+	if err := s.ensureConnections(); err != nil {
+		return err
+	}
+
+	// declare all the subscriber things!
+	if len(s.consumerDecls) > 0 {
+		ch, _ := s.consumeConn.Channel()
+		for _, declare := range s.consumerDecls {
+			if err := declare(ch); err != nil {
+				return fmt.Errorf("failed to declare for consumer: %s", err.Error())
+			}
+		}
+	}
+
+	// declare all the consumer things!
+	if len(s.producerDecls) > 0 {
+		ch, _ := s.produceConn.Channel()
+		for _, declare := range s.producerDecls {
+			if err := declare(ch); err != nil {
+				return fmt.Errorf("failed to declare for producer: %s", err.Error())
+			}
+		}
+	}
+
+	return nil
+}
+
+// Shutdown all existing connections but wait for any in-flight messages to be processed first.
+// Finally, the session context is cancelled which will stop any child-goroutines.
+func (s *Session) Shutdown() {
+	defer s.cancel()
+
+	if s.consumeConn != nil {
+		s.consumeConn.Shutdown()
+		s.logger.Info("amqp consumer connection closed")
+	}
+	if s.produceConn != nil {
+		s.produceConn.Shutdown()
+		s.logger.Info("amqp producer connection closed")
+	}
+}
+
+func (s *Session) Consume() {
+	for {
+		select {
+		case <-s.ctx.Done():
+			return
+		default:
+		}
+
+		if !s.consumeConn.IsConnected() {
+			s.logger.Info("consuming halted: connection offline")
+			time.Sleep(5 * time.Second)
+			continue
+		}
+
+		ch, err := s.consumeConn.Channel()
+		if err != nil {
+			s.logger.Error("failed to fetch channel", zap.Error(err))
+			continue
+		}
+
+		_ = ch.Qos(10, 0, false)
+
+		deliveries, err := ch.Consume(s.consumerQueue, "", false, false, false, false, nil)
+		if err != nil {
+			s.logger.Error("consumer error", zap.Error(err))
+			continue
+		}
+
+		for delivery := range deliveries {
+			routingKey := delivery.RoutingKey
+			s.logger.Info("incoming amqp delivery", zap.String("routingKey", routingKey))
+			if handler, ok := s.subscribers[routingKey]; ok {
+				handler(delivery)
+			} else {
+				s.logger.Error("delivery has routing key which cannot be processed, NACKing")
+				_ = delivery.Nack(false, false)
+			}
+		}
+	}
+}
